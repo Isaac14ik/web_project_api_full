0fbede2b7af24602d880397c2830f44a563ca340
Jorge Nava <jorgesrisaac@gmail.com>
2026-07-13 21:19:01 -0600

    refactor: actualizar BASE_URL en auth.js para conectar con backend local

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://se-register-api.en.tripleten-services.com/v1';
+export const BASE_URL = 'http://localhost:3000';
 
 const checkResponse = (res) => {
   if (res.ok) {
