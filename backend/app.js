94a21e6a42a0eec6998f82112fceaec1b5fe49c4
Jorge Nava <jorgesrisaac@gmail.com>
2026-07-13 20:18:00 -0600

    feat: configurar servidor app.js con rutas signin y signup

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { login, createUser } = require('./controllers/users');
+
+const app = express();
+const { PORT = 3000 } = process.env;
+
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/mestodb')
+  .then(() => console.log('Conectado con éxito a MongoDB'))
+  .catch((err) => console.log('Error al conectar a MongoDB:', err));
+
+app.post('/signin', login);
+app.post('/signup', createUser);
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
