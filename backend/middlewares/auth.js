6cd3d92e7de1f10f25efbf1527b69fb08644d19b
Jorge Nava <jorgesrisaac@gmail.com>
2026-07-13 20:32:11 -0600

    feat: actualizar middleware auth para retornar error 403

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,7 +4,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res.status(401).send({ message: 'Se requiere autorización' });
+    return res.status(403).send({ message: 'Se requiere autorización' });
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -13,7 +13,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, 'super-strong-secret-key');
   } catch (err) {
-    return res.status(401).send({ message: 'Se requiere autorización' });
+    return res.status(403).send({ message: 'Se requiere autorización' });
   }
 
   req.user = payload;
