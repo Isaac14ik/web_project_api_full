1dee351c2fd5b1e4aa0188ec01dba5ffd3a97fa3
Jorge Nava <jorgesrisaac@gmail.com>
2026-07-13 21:23:11 -0600

    chore: integrar regla no-unused-vars para next en configuracion de eslint

--- frontend/src/eslint.config.js
+++ frontend/src/eslint.config.js
@@ -2,7 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import react from 'eslint-plugin-react' // Asegúrate de que esta línea esté o instálalo
+import react from 'eslint-plugin-react'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -10,7 +10,7 @@ export default defineConfig([
   {
     files: ['**/*.{js,jsx}'],
     plugins: {
-      react, // Añadimos el plugin de react si no estaba
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
@@ -18,15 +18,15 @@ export default defineConfig([
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
-    // AQUÍ AGREGAMOS LAS REGLAS
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      'no-unused-vars': ['error', { 'argsIgnorePattern': 'next' }],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
-      'react/prop-types': 'off', // <--- ESTA ES LA QUE BUSCAMOS
+      'react/prop-types': 'off',
       'react/jsx-no-target-blank': 'off',
     },
   },
