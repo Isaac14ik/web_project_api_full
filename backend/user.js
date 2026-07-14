83d72f32eacc5b3e51440dc0386af28bb14bee6a
Jorge Nava <jorgesrisaac@gmail.com>
2026-07-13 20:01:00 -0600

    feat: actualizar esquema de usuario con credenciales y valores por defecto

--- backend/user.js
+++ backend/user.js
@@ -2,6 +2,26 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    default: 'Jacques Cousteau',
+    minlength: 2,
+    maxlength: 30,
+  },
+  about: {
+    type: String,
+    default: 'Explorador',
+    minlength: 2,
+    maxlength: 30,
+  },
+  avatar: {
+    type: String,
+    default: 'https://amazonaws.com',
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Formato de enlace de avatar incorrecto',
+    },
+  },
   email: {
     type: String,
     required: true,
