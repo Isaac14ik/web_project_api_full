20c0b8c004ce4070938110d8754fdcd917f3afce
Jorge Nava <jorgesrisaac@gmail.com>
2026-07-18 12:58:52 -0600

    feat: implementar ruta crash-test para pm2

--- backend/app.js
+++ backend/app.js
@@ -24,6 +24,12 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb')
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('El servidor va a caer');
+  }, 0);
+});
+
 app.post('/signin', validateUserSignin, login);
 app.post('/signup', validateUserSignup, createUser);
 
