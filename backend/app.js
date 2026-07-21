ad3bde330176a8f66d264e3acb5b8f6a4278c910
Jorge Nava <jorgesrisaac@gmail.com>
2026-07-20 20:41:57 -0600

    modifique app.js

--- backend/app.js
+++ backend/app.js
@@ -15,10 +15,8 @@ const app = express();
 const { PORT = 3000, MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 app.use(express.json());
-
-
 app.use(cors());
-app.options('/:any*', cors()); 
+app.options('/{*corsSplat}', cors());
 
 mongoose.connect(MONGO_URL)
   .then(() => console.log('Conectado con éxito a MongoDB'))
@@ -40,8 +38,7 @@ app.use(auth);
 app.use('/users', userRoutes);
 app.use('/cards', cardRoutes);
 
-
-app.use((req, res) => {
+app.all('/{*pathSplat}', (req, res) => {
   res.status(404).send({ message: 'Recurso no encontrado' });
 });
 
